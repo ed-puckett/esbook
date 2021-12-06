8faee20d50c658f92b998e6fcb8e925b3fcd2a84
Ed Puckett <ed.puckett@gmail.com>
2021-12-05 20:46:27 -0700

    minor updates

--- src/facet/fs-interface.js
+++ src/facet/fs-interface.js
@@ -35,34 +35,37 @@
 
         /** Load JSON from the file associated with a FileSystemFileHandle
          *  @param {FileSystemFileHandle} file_handle
-         *  @param {boolean} verify_for_writing verify write permissions, too (defaults to false)
+         *  @param {boolean} verify_for_writing (Default false) If true, verify write permissions, too
          *  @return {Promise} resolves to { text: string, fs_timestamp: number }
          */
         async load_raw(file_handle, verify_for_writing=false) {
             await this.verify_permission(file_handle, verify_for_writing);
             const file = await file_handle.getFile();
+            const text = await file.text();
+            const fs_timestamp = file.lastModified;
             return {
-                text: await file.text(),
-                fs_timestamp: file.lastModified,
+                text,
+                fs_timestamp,
             };
         }
 
         /** Load an object that is encoded in JSON from the file associated with a FileSystemFileHandle
          *  @param {FileSystemFileHandle} file_handle
-         *  @param {boolean} verify_for_writing verify write permissions, too (defaults to false)
+         *  @param {boolean} verify_for_writing (Default false) If true, verify write permissions, too
          *  @return {Promise} resolves to { contents: object, fs_timestamp: number }
          */
         async load_json(file_handle, verify_for_writing=false) {
             const { text, fs_timestamp } = await this.load_raw(file_handle, verify_for_writing);
+            const contents = JSON.parse(text);
             return {
-                contents: JSON.parse(text),
+                contents,
                 fs_timestamp,
             };
         }
 
         /** Return the "last modified" timestamp for the file associated with a FileSystemFileHandle
          *  @param {FileSystemFileHandle} file_handle
-         *  @return {Promise} which resolves to the "last modified" time of the file,
+         *  @return {Promise} resolves to the "last modified" time of the file,
          *                    in milliseconds since the UNIX epoch (January 1, 1970 at Midnight UTC)
          */
         async get_fs_timestamp(file_handle) {
@@ -72,35 +75,24 @@
         }
 
         /** Show a file picker for the user to select a file for saving
-         *  @return {Promise} which resolves to { canceled: true }|{ file_handle: FileSystemFileHandle, fs_timestamp: number }
+         *  @return {Promise} resolves to { canceled: true }|{ file_handle: FileSystemFileHandle, fs_timestamp: number }
          */
         async prompt_for_save() {
-            const options = {};
-            let file_handle;
-            try {
-                file_handle = await window.showSaveFilePicker(options);
-            } catch (err) {
-                if (err instanceof AbortError) {
-                    return { canceled: true };
-                } else {
-                    throw err;
-                }
-            }
-            const fs_timestamp = (await file_handle.getFile()).lastModified;
-            return {
-                file_handle,
-                fs_timestamp,
-            };
+            return _prompt(window.showSaveFilePicker);
         }
 
         /** Show a file picker for the user to select a file for loading
-         *  @return {Promise} which resolves to { canceled: true }|{ file_handle: FileSystemFileHandle, fs_timestamp: number }
+         *  @return {Promise} resolves to { canceled: true }|{ file_handle: FileSystemFileHandle, fs_timestamp: number }
          */
         async prompt_for_load() {
+            return _prompt(window.showOpenFilePicker);
+        }
+
+        async _prompt(picker) {
             const options = {};
             let file_handle;
             try {
-                file_handle = await window.showOpenFilePicker(options);
+                file_handle = await picker(options);
             } catch (err) {
                 if (err instanceof AbortError) {
                     return { canceled: true };
