a36fa1cf8528b0232d2bd6eee84892cd743c90e9
Ed Puckett <ed.puckett@gmail.com>
2021-12-11 15:41:16 -0700

    updated implementation of facet/key-bindings.js

--- src/facet/key-bindings.js
+++ src/facet/key-bindings.js
@@ -4,6 +4,9 @@
 
     const define_subscribable = await facet('facet/subscribable.js');
 
+
+    // === COMMANDS AND KEY BINDINGS ===
+
     const commands = [
         'undo',
         'redo',
@@ -48,24 +51,109 @@
         'CmdOrCtrl+Alt+Backspace':  'delete_element',
     };
 
-    const is_on_macos = (navigator.platform ?? navigator.userAgentData.platform ?? '').toLowerCase().startsWith('mac');
 
-    const CmdOrCtrl_event_field = is_on_macos ? 'metaKey' : 'ctrlKey';
+    // === KEY BINDING PARSING ===
 
-    function key_spec_modifier_to_event_field(modifier) {
-        switch (modifier.toLowerCase()) {
-        case 'cmdorctrl':     return CmdOrCtrl_event_field;
-        case 'commandorctrl': return CmdOrCtrl_event_field;
-        case 'ctrl':          return 'ctrlKey';
-        case 'shift':         return 'shiftKey';
-        case 'cmd':           return 'metaKey';
-        case 'command':       return 'metaKey';
-        case 'meta':          return 'metaKey';
-        case 'alt':           return 'altKey';
-        default: return undefined;
+    // basic_modifier_desc_map is the definition from which
+    // modifier_desc_map and modifier_code_desc_map are derived.
+    const basic_modifier_desc_map = {
+        ctrl:  { code: 'c', event_prop: 'ctrlKey',  alternates: [ 'control' ] },
+        shift: { code: 's', event_prop: 'shiftKey', alternates: [] },
+        meta:  { code: 'm', event_prop: 'metaKey',  alternates: ['cmd', 'command' ] },
+        alt:   { code: 'a', event_prop: 'altKey',   alternates: [] },
+    };
+
+    const canonical_key_spec_separator = '+';  // separator between modifier codes and key in a canonical key spec
+    const disallowed_modifier_codes = '+-' + canonical_key_spec_separator;
+
+    function build_modifier_desc_map() {
+        // validate basic_modifier_desc_map:
+        {
+            const keys = Object.keys(basic_modifier_desc_map);
+            if (keys.some(k => k !== k.toLowerCase())) {
+                throw new Error('basic_modifier_desc_map keys must be lowercase');
+            }
+            const all_alternates = keys.map(k => basic_modifier_desc_map[k].alternates).reduce((acc, a) => [...acc, ...a], []);
+            if (all_alternates.some(k => k !== k.toLowerCase())) {
+                throw new Error('basic_modifier_desc_map alternates must be lowercase');
+            }
+            if (new Set([...keys, ...all_alternates]).size !== (keys.length + all_alternates.length)) {
+                throw new Error('basic_modifier_desc_map keys and alternates must all be distinct');
+            }
+            const codes = keys.map(k => basic_modifier_desc_map[k].code)
+            for (const code of codes) {
+                if (code.length !== 1) {
+                    throw new Error('basic_modifier_desc_map codes must be single characters');
+                }
+                if (disallowed_modifier_codes.includes(code)) {
+                    throw new Error(`basic_modifier_desc_map codes are not allowed to be any of following: ${disallowed_modifier_codes}`);
+                }
+            }
+            if (new Set(codes).size !== codes.length) {
+                throw new Error('basic_modifier_desc_map code values must be distinct');
+            }
+            const props = keys.map(k => basic_modifier_desc_map[k].event_prop)
+            if (new Set(props).size !== props.length) {
+                throw new Error('basic_modifier_desc_map event_prop values must be distinct');
+            }
         }
+        // validation passed; build the map
+        const mdm = {};
+        function create_extended_desc(basic_modifier_key, modifier_key, desc) {
+            const ext_desc = {
+                modifier: modifier_key,
+                basic_modifier: basic_modifier_key,
+                ...desc,
+                alternates: [ ...new Set([ basic_modifier_key, modifier_key, ...desc.alternates ]) ],
+            };
+            return Object.freeze(ext_desc);
+        }
+        for (const bmdm_key in basic_modifier_desc_map) {
+            const desc = basic_modifier_desc_map[bmdm_key];
+            mdm[bmdm_key] = create_extended_desc(bmdm_key, bmdm_key, desc);
+            for (const alt_key of desc.alternates) {
+                mdm[alt_key] = create_extended_desc(bmdm_key, alt_key, desc);
+            }
+        }
+        return Object.freeze(mdm);
     }
 
+    // modifier_desc: {
+    //     modifier:       string,  // modifier string
+    //     basic_modifier: string,  // canonical modifier string
+    //     code:           string,  // canonical code for modifier
+    //     event_prop:     string,  // corresponding property in KeyboardEvent object
+    //     alternates:     string,  // all alternates, including basic_modifier
+    // }
+    const modifier_desc_map = build_modifier_desc_map();  // modifier_string->modifier_desc
+    const modifier_code_desc_map =  // modifier_code->modifier_desc
+          Object.freeze(
+              Object.fromEntries(
+                  Object.keys(basic_modifier_desc_map)
+                      .map(k => modifier_desc_map[k])
+                      .map(desc => [ desc.code, desc ])
+              )
+          );
+
+    const is_on_macos = (navigator.platform ?? navigator.userAgentData.platform ?? '').toLowerCase().startsWith('mac');
+
+    const CmdOrCtrl = is_on_macos ? 'meta' : 'ctrl';
+
+    function key_spec_modifier_to_desc(modifier) {
+        modifier = modifier.toLowerCase();
+        if (['cmdorctrl', 'commandorctrl'].includes(modifier)) {
+            modifier = CmdOrCtrl.toLowerCase();
+        }
+        return modifier_desc_map[modifier];
+    }
+
+    function _modifier_descs_and_key_to_canoncial_key_spec(modifier_descs, key) {
+        const canonical_modifiers = modifier_descs.map(desc => desc.code).sort().join('');
+        const canonical_key_spec = `${canonical_modifiers}${canonical_key_spec_separator}${key}`;
+        return canonical_key_spec;
+    }
+
+    // parse_key_spec() returns a canonical key spec (which is a string)
     function parse_key_spec(key_spec) {
         if (typeof key_spec !== 'string') {
             throw new Error('key_spec must be a string');
@@ -74,48 +162,50 @@
         if (modifiers.length < 1 || modifiers.some(s => s.length <= 0)) {
             throw new Error('invalid key_spec');
         }
-        let key = modifiers[modifiers.length-1];
+        let key = modifiers[modifiers.length-1];  // note: already converted to lowercase above
         if (['up', 'down', 'left', 'right'].includes(key)) {
             key = `arrow${key}`;
         }
         modifiers.splice(modifiers.length-1, 1);
-        const modifier_event_fields = {};
+        const modifier_descs = [];
         for (const modifier of modifiers) {
-            const event_field = key_spec_modifier_to_event_field(modifier);
-            if (!event_field) {
+            const desc = key_spec_modifier_to_desc(modifier);
+            if (!desc) {
                 throw new Error(`invalid modifier "${modifier}" in key_spec ${key_spec}`);
             }
-            if (event_field in modifier_event_fields) {
+            if (desc.code in modifier_descs) {
                 throw new Error(`redundant modifier "${modifier}" in key_spec ${key_spec}`);
             }
-            modifier_event_fields[event_field] = true;
+            modifier_descs.push(desc);
         }
-        return { key, modifier_event_fields };
+        return _modifier_descs_and_key_to_canoncial_key_spec(modifier_descs, key);
     }
 
-    const key_bindings = Object.entries(key_bindings_specs).map(([ key_spec, command ]) => [ parse_key_spec(key_spec), command ]);
+    const key_bindings =  // array of [ canonical_key_spec, command ] elements
+          Object.freeze(
+              Object.entries(key_bindings_specs)
+                  .map(([ key_spec, command ]) => [ parse_key_spec(key_spec), command ])
+          );
 
-    function compare_key_event(event, key, modifier_event_fields) {
-        if (event.key.toLowerCase() !== key) {
-            return false;
-        }
-        for (const event_field of [
-            'ctrlKey',
-            'shiftKey',
-            'metaKey',
-            'altKey',
-        ]) {
-            if (!!event[event_field] !== !!modifier_event_fields[event_field]) {
-                return false;
+    // parse_event() returns a canonical key spec (which is a string)
+    function parse_keyboard_event(keyboard_event) {
+        const modifier_descs = [];
+        for (const modifier in basic_modifier_desc_map) {
+            const desc = basic_modifier_desc_map[modifier];
+            if (keyboard_event[desc.event_prop]) {
+                modifier_descs.push(desc);
             }
         }
-        return true;
+        return _modifier_descs_and_key_to_canoncial_key_spec(modifier_descs, event.key.toLowerCase());
     }
+//!!! need to compare key case-insensitive if modifiers exist, otherwise case-sensitive
 
-    function key_event_to_command(event) {
-        for (const [ { key, modifier_event_fields }, command ] of key_bindings) {
-            if (compare_key_event(event, key, modifier_event_fields))
+    function keyboard_event_to_command(keyboard_event) {
+        const event_canonical_key_spec = parse_keyboard_event(keyboard_event);
+        for (const [ canonical_key_spec, command ] of key_bindings) {
+            if (canonical_key_spec === event_canonical_key_spec) {
                 return command;
+            }
         }
         return undefined;
     }
@@ -124,11 +214,11 @@
         commands,
         key_bindings_specs,
         is_on_macos,
-        CmdOrCtrl_event_field,
-        parse_key_spec,
+        CmdOrCtrl,
         key_bindings,
-        compare_key_event,
-        key_event_to_command,
+        modifier_desc_map,
+        modifier_code_desc_map,
+        keyboard_event_to_command,
     });
 
 } catch (err) { facet_load_error(err, current_script); } })(facet_init());  // facet end
